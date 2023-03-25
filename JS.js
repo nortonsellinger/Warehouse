19c6942b123bd206e3a5fe867d263b30028b34d5
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-25 22:09:19 +0800

    k

--- JS.js
+++ JS.js
@@ -7,5 +7,11 @@ const dvc_ratio = dvc_width / dvc_height;
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --DEVICE_RATIO В CSS ВЫЧИСЛЕННОЕ СООТНОШЕНИЕ СТОРОН УСТРОЙСТВА
 document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --NAVWIDTH В CSS ЗНАЧЕНИЕ ШИРИНЫ ПАНЕЛИ НАВИГАЦИИ
-let NAVWIDTH = document.querySelector('nav').offsetWidth;
-document.documentElement.style.setProperty('--NAVWIDTH', NAVWIDTH + 'px');
+let NAV_WIDTH = document.querySelector('nav').offsetWidth;
+document.documentElement.style.setProperty('--NAV_WIDTH', NAV_WIDTH + 'px');
+
+let IMAGE_WIDTH =
+Math.min(NAV_WIDTH, document.querySelector('.headerlogo').style.getPropertyValue(max-height)) - 
+document.querySelector('header').style.getPropertyValue(--PADDING) * 2;
+
+document.querySelector('.headerimg').style.setProperty('--IMAGE_WIDTH', NIMAGE_WIDTH + 'px');
