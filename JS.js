b4afb243eb68b5901999732bfa92c52a68c57e35
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-25 17:56:34 +0800

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,8 @@
+//ПОЛУЧАЕМ ШИРИНУ ЭКРАНА
+const dvc_width = window.screen.width;
+//ПОЛУЧАЕМ ВЫСОТУ ЭКРАНА
+const dvc_height = window.screen.height;
+//ВЫЧИСЛЯЕМ ОТНОШЕНИЕ !ШИРИНЫ! К !ВЫСОТЕ!
+const dvc_ratio = dvc_width / dvc_height;
+//ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --DEVICE_RATIO В CSS ВЫЧИСЛЕННОЕ СООТНОШЕНИЕ СТОРОН УСТРОЙСТВА
+document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
