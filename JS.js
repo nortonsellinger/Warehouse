e5900e9734a8a1b730d26a530d8b731a184b7373
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-25 19:03:05 +0800

    u

--- JS.js
+++ JS.js
@@ -6,3 +6,7 @@ const dvc_height = window.screen.height;
 const dvc_ratio = dvc_width / dvc_height;
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --DEVICE_RATIO В CSS ВЫЧИСЛЕННОЕ СООТНОШЕНИЕ СТОРОН УСТРОЙСТВА
 document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
+//ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --NAVWIDTH В CSS ЗНАЧЕНИЕ ШИРИНЫ ПАНЕЛИ НАВИГАЦИИ
+let NAVWIDTH = document.querySelector('nav').offsetWidth;
+document.documentElement.style.setProperty('--NAVWIDTH', NAVWIDTH + 'px');
+alert(NAVWIDTH);
