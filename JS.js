b3db38a34795f7797adf9083618333a80940e617
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-26 22:49:13 +0800

    Update JS.js

--- JS.js
+++ JS.js
@@ -9,13 +9,18 @@ document.documentElement.style.setProperty('--DEVICE_RATIO', 'device_ratio');
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --NAVWIDTH В CSS ЗНАЧЕНИЕ ШИРИНЫ ПАНЕЛИ НАВИГАЦИИ
 var NAV_WIDTH = document.querySelector('nav').offsetWidth;
 document.documentElement.style.setProperty('--NAV_WIDTH', NAV_WIDTH + 'px');
+
 //УСТАНАВЛИВАЕМ ПЕРЕМЕННУЮ ПАДДИНГА ДЛЯ ХЕДЕРА
 var HEADER_PADDING = 6;
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --PADDING В CSS ЗНАЧЕНИЕ ПЕРЕМЕННОЙ HEADER_PADDING
 document.querySelector('header').style.setProperty('--PADDING', HEADER_PADDING + 'px');
-//УСТАНАВИВАЕМ ПЕРЕМЕННУЮ ШИРИНЫ КАРТИНКИ ДЛЯ ХЕДЕРА
-let IMAGE_WIDTH =
-Math.min(NAV_WIDTH, 0.05 * Math.max(screen.width, screen.height)) - 
+var IMAGE_WIDTH;
+window.addEventListener("resize", RatioChange());
+function RatioChange() {
+    IMAGE_WIDTH = Math.min(NAV_WIDTH, 0.05 * Math.max(window.innerWidth, window.innerHeight)) - 
 HEADER_PADDING * 2;
+    document.querySelector('.headerimg').style.setProperty('--IMAGE_WIDTH', IMAGE_WIDTH + 'px');
+}
+//УСТАНАВИВАЕМ ПЕРЕМЕННУЮ ШИРИНЫ КАРТИНКИ ДЛЯ ХЕДЕРА
+
 //ПРИСВАИВАЕМ ПЕРЕМЕННОЙ --IMAGE_WIDTH В CSS ЗНАЧЕНИЕ ПЕРЕМЕННОЙ IMAGE_WIDTH
-document.querySelector('.headerimg').style.setProperty('--IMAGE_WIDTH', IMAGE_WIDTH + 'px');
