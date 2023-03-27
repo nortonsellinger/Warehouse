4e1ca41aceb31a5166755d3ed5016f4393a746d1
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-27 21:43:11 +0800

    sas

--- JS.js
+++ JS.js
@@ -15,6 +15,8 @@ document.documentElement.style.setProperty('--NAV_WIDTH', NAV_WIDTH + 'px');
 
 var max_header_coeff = 0.05;
 var header_pad_coeff = 0.06;
+var header_padding;
+var TITLE_PADDING;
 var MAX_HEADER_HEIGHT;
 var HEADER_FONT_SIZE;
 var IMAGE_WIDTH;
@@ -24,8 +26,11 @@ function DefinePageParams() {
 
     header_padding = header_pad_coeff * MAX_HEADER_HEIGHT;
     document.querySelector('header').style.setProperty('--PADDING', header_padding + 'px');
+
+    TITLE_PADDING = header_padding + 6;
+    document.querySelector('.headertitle').style.setProperty('--TITLE_PADDING', TITLE_PADDING + 'px');
     
-    HEADER_FONT_SIZE = MAX_HEADER_HEIGHT - header_padding * 2;
+    HEADER_FONT_SIZE = MAX_HEADER_HEIGHT - TITLE_PADDING * 2;
     document.querySelector('header').style.setProperty('--HEADER_FONT_SIZE', HEADER_FONT_SIZE + 'px');
 
     IMAGE_WIDTH = Math.min(NAV_WIDTH, MAX_HEADER_HEIGHT) - header_padding * 2;
