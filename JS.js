bd37f7e1835ead2f13ccbeccf1044338aa872120
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-28 18:32:00 +0800

    ш

--- JS.js
+++ JS.js
@@ -38,4 +38,9 @@ function DefinePageParams() {
 
 DefinePageParams();
 
-window.addEventListener("resize", DefinePageParams());
+window.addEventListener('resize', DefinePageParams());
+
+function Nav_Visibility() {
+    document.querySelector('nav').style.setProperty('visibility', 'visible');
+}
+document.querySelector('.headerimg').addEventListener('click', Nav_Visibility());
