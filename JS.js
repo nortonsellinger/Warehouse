0c5f49cf31098e4a4fbafd6aa29c413266a65478
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-28 20:51:42 +0800

    Update JS.js

--- JS.js
+++ JS.js
@@ -48,10 +48,10 @@ document.querySelector('.headerimg').addEventListener('click', Nav_Visibility())
 var grad_colors = ['red', 'yellow', 'blue'];
 var grad_direct = '-45deg, ';
 var grad_space = 100 / grad_colors.length;
-var gradient;
+var gradient = '';
 let i = 0;
 while (i <= grad_colors.length - 1) {
-    gradient = grad_colors[i] + ' ' + grad_space * i + '%, ';
+    gradient += grad_colors[i] + ' ' + grad_space * i + '%, ';
 }
 gradient = gradient.slice(0, -2);
 document.getElementById('gradient_anim').style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient ')');
