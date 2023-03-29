e1fa15131e1c18bc0a5e49e37bcf2bbea6a0613c
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-03-29 19:35:05 +0800

    YAYY IT WORKS

--- JS.js
+++ JS.js
@@ -3,25 +3,26 @@
 }
 document.querySelector('.headerimg').addEventListener('click', Nav_Visibility());*/
 
-var grad_colors = ['red', 'yellow', 'lime', 'aqua', 'blue'];
+var grad_colors = new Array("red", "yellow", "lime", "aqua", "blue");
 var grad_direct = '45deg, ';
-var grad_space = 100 / grad_colors.length;
+var grad_space = 100 / (grad_colors.length - 1);
 var gradient = '';
 for (i = 0; i < grad_colors.length; i++) {
     gradient += grad_colors[i] + ' ' + grad_space * i + '%, ';
 }
 gradient = gradient.slice(0, -2);
-document.getElementById('gradient_anim').style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
+document.querySelector('.gradient_anim').style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
 
-function GradientAnimation {
-    for (i = 0; i < grad_colors.length; i++) {
-        grad_colors.unshift(grad_colors.pop());
-    }
-    gradient = ' ';
+
+function GradientAnimation () {
+    grad_colors.unshift(grad_colors.pop(grad_colors[grad_colors.length - 1]));
+    gradient = '';
     for (i = 0; i < grad_colors.length; i++) {
         gradient += grad_colors[i] + ' ' + grad_space * i + '%, ';
     }
     gradient = gradient.slice(0, -2);
-    document.getElementById('gradient_anim').style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
+    document.querySelector('.gradient_anim').style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
 }
-setInterval(GradientAnimation, 100);
+
+gradient_anim = setInterval(GradientAnimation, 100);
+/*clearInterval(gradient_anim);*/
