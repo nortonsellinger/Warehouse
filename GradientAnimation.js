813554d17cec94723222f1679540740017175165
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-04-01 20:46:06 +0800

    y

--- GradientAnimation.js
+++ GradientAnimation.js
@@ -2,26 +2,21 @@ var grad_colors = new Array('red', 'orange', 'yellow', 'lime', 'aqua', 'blue', '
 var grad_direct = '135deg, ';
 var grad_space = 100 / (grad_colors.length - 1);
 var gradient = '';
-for (i = 0; i < grad_colors.length; i++) {
-    gradient += grad_colors[i] + ' ' + grad_space * i + '%, ';
-}
-gradient = gradient.slice(0, -2);
-
 const gradClass = document.querySelectorAll('.gradient_anim');
 gradClass.forEach(function (el) {
     el.style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
 });
 
 function GradientAnimation () {
-    grad_colors.unshift(grad_colors.pop(grad_colors[grad_colors.length - 1]));
-    gradient = '';
     for (i = 0; i < grad_colors.length; i++) {
         gradient += grad_colors[i] + ' ' + grad_space * i + '%, ';
     }
     gradient = gradient.slice(0, -2);
     gradClass.forEach(function (el) {
     el.style.setProperty('background', 'linear-gradient' + '(' + grad_direct + gradient + ')');
-});
+    });
+    grad_colors.unshift(grad_colors.pop(grad_colors[grad_colors.length - 1]));
+    gradient = '';
 }
 
 requestAnimationFrame(setInterval(GradientAnimation, 150));
