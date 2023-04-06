a253b336049aa3f961edbfa68b08e6be9568d319
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-04-06 13:23:49 +0800

    Update PageSetup.js

--- PageSetup.js
+++ PageSetup.js
@@ -12,24 +12,24 @@ var HeaderFontSize = MaxHeaderHeight - TitlePadding * 2;
 var SectionPaddingVer = 0.02 * MinorEdge;
 var SectionPaddingHor = 0.02 * GreaterEdge;
 
-document.querySelector('footer').style.setProperty('padding', HeaderPadding * 2 + 'px'); 
-document.querySelector('.headertitle').style.setProperty('padding', TitlePadding + 'px');
-document.querySelector('.headertitle').style.setProperty('padding-left', TitlePadding * 1.2 + 'px');
-document.querySelector('.headertitle').style.setProperty('font-size', HeaderFontSize + 'px');
-document.querySelector('footer').style.setProperty('font-size', HeaderFontSize * 0.7 + 'px');
-document.querySelector('.headerlogo').style.setProperty('height', MaxHeaderHeight + 'px');
-document.querySelector('.headerlogo').style.setProperty('width', MaxHeaderHeight + 'px');
-document.querySelector('.headerlogo').style.setProperty('padding', HeaderPadding + 'px');
-document.querySelector('.headerimg').style.setProperty('width', LogoWidth + 'px');
-document.getElementById('content').style.setProperty('font-size', BaseContentFontSize + 'px');
+document.querySelector('footer').style.padding = HeaderPadding * 2 + 'px'; 
+document.querySelector('.headertitle').style.padding = TitlePadding + 'px';
+document.querySelector('.headertitle').style.paddingLeft = TitlePadding * 1.2 + 'px';
+document.querySelector('.headertitle').style.fontSize = HeaderFontSize + 'px';
+document.querySelector('footer').style.fontSize = HeaderFontSize * 0.7 + 'px';
+document.querySelector('.headerlogo').style.height = MaxHeaderHeight + 'px';
+document.querySelector('.headerlogo').style.width = MaxHeaderHeight + 'px';
+document.querySelector('.headerlogo').style.padding = HeaderPadding + 'px';
+document.querySelector('.headerimg').style.width = LogoWidth + 'px';
+document.getElementById('content').style.fontSize = BaseContentFontSize + 'px';
 
 function PageSetup() {
-    document.querySelector('section').style.setProperty('padding', 0.02 * screen.width + 'px');
+    document.querySelector('section').style.padding = 0.02 * screen.width + 'px';
 }
 
 PageSetup();
 
 /*window.addEventListener('resize', PageSetup, true);*/
 window.addEventListener("orientationchange", function() {
-    document.querySelector('section').style.setProperty('padding', 0.02 * screen.width + 'px');
+    document.querySelector('section').style.padding = 0.02 * screen.width + 'px';
 }, false);
