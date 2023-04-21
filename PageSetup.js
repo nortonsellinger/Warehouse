3bd33c951a653220b5ddc76f2c69ee911b9ac843
Norton Sellinger <47449946+nortonsellinger@users.noreply.github.com>
2023-04-21 21:24:43 +0800

    Update PageSetup.js

--- PageSetup.js
+++ PageSetup.js
@@ -2,42 +2,32 @@ const GreaterEdge = Math.max(screen.height, screen.width);
 const MinorEdge = Math.min(screen.height, screen.width);
 const Ratio = GreaterEdge / MinorEdge;
 
-var MaxHeaderHeight = 0.05 * GreaterEdge;
-var BaseContentFontSize = GreaterEdge * Ratio * 0.008;
-var HeaderPadding = 0.06 * MaxHeaderHeight;
+const MaxHeaderHeight = 0.05 * GreaterEdge;
+const HeaderPadding = 0.06 * MaxHeaderHeight;
+
 var LogoWidth = MaxHeaderHeight - HeaderPadding * 2;
 var TitlePadding = HeaderPadding * 2;
 var HeaderFontSize = MaxHeaderHeight - TitlePadding * 2;
 
-var ContentImgMaxHeight = 0.25 * screen.height;
-/*var contentImgs = document.querySelectorAll('.content_img img');
-
-*/
-document.querySelector('.title').style.fontSize = BaseContentFontSize * 1.3+ 'px';
-/*var titleSides = document.querySelectorAll('.title_side');
-titleSides.forEach (
-	function(side) {
-		side.style.fontSize = side.offsetHeight + 'px';
-	}
-)*/
-
-document.querySelector('footer').style.padding = HeaderPadding * 2 + 'px'; 
 document.querySelector('.headertitle').style.padding = TitlePadding + 'px';
 document.querySelector('.headertitle').style.paddingLeft = TitlePadding * 1.2 + 'px';
 document.querySelector('.headertitle').style.fontSize = HeaderFontSize + 'px';
-document.querySelector('footer').style.fontSize = HeaderFontSize * 0.7 + 'px';
 document.querySelector('.headerlogo').style.height = MaxHeaderHeight + 'px';
 document.querySelector('.headerlogo').style.width = MaxHeaderHeight + 'px';
 document.querySelector('.headerlogo').style.padding = HeaderPadding + 'px';
 document.querySelector('.headerimg').style.width = LogoWidth + 'px';
+document.querySelector('footer').style.fontSize = HeaderFontSize * 0.7 + 'px';
+document.querySelector('footer').style.padding = HeaderPadding * 2 + 'px'; 
 
+var BaseContentFontSize = GreaterEdge * Ratio * 0.008;
 if (BaseContentFontSize > 20) {
     BaseContentFontSize = 20;
 }
+document.querySelector('.title').style.fontSize = BaseContentFontSize * 1.3 + 'px';
 document.querySelector('section').style.fontSize = BaseContentFontSize + 'px';
 document.querySelector('nav').style.fontSize = BaseContentFontSize * 1.2 + 'px';
 
-ContentImgMaxHeight = 0.25 * screen.height;
+var ContentImgMaxHeight = 0.25 * screen.height;
 var contentImgs = document.querySelectorAll('.content_img img');
 
 function PageSetup() {
